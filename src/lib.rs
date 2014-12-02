cfaa3384ba21fe6587892f3a503ef47fc2acf6d6
Jonathan Reem <jonathan.reem@gmail.com>
2014-12-01 20:28:33 -0500

    (feat) debug_unreachable! macro.

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![license = "MIT"]
+#![deny(missing_docs, warnings)]
+#![feature(macro_rules)]
+
+//! `unreachable!()` in debug builds, `intrinsics::unreachable` in release.
+
+#[macro_export]
+/// `unreachable!()` in debug builds, `intrinsics::unreachable` in release.
+macro_rules! debug_unreachable {
+    () => {
+        if cfg!(ndebug) {
+            ::std::intrinsics::unreachable()
+        } else {
+            unreachable!()
+        }
+    }
+}
+
