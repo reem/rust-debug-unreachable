c7a34c41abdf6f017b5f677199c1ed2a9fc6b1e8
Jonathan Reem <jonathan.reem@gmail.com>
2015-06-02 18:55:15 -0400

    (feat) Run on stable rust.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 #![deny(missing_docs, warnings)]
 
-//! `panic!()` in debug builds, `intrinsics::unreachable` in release.
+//! `panic!()` in debug builds, optimization hint in release.
+
+extern crate unreachable;
+
+#[doc(hidden)]
+pub use unreachable::unreachable as __unreachable;
 
 #[macro_export]
-/// `panic!()` in debug builds, `intrinsics::unreachable` in release.
+/// `panic!()` in debug builds, optimization hint in release.
 macro_rules! debug_unreachable {
     () => { debug_unreachable!("entered unreachable code") };
     ($e:expr) => {
         if cfg!(ndebug) {
-            ::std::intrinsics::unreachable();
+            $crate::__unreachable()
         } else {
             panic!($e);
         }
